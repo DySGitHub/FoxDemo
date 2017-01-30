ae878c76b7717e1473e47be0891930d0201878c9
Dylan Sage <20063614@mail.wit.ie>
2017-01-30 14:19:19 +0000

    User microposting feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
      before_save { self.email = email.downcase }
+    has_many :microposts, dependent: :destroy 
      validates :name, presence: true, length: { in: 9..30 }
       VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
       validates :email, presence: true, 
@@ -7,4 +8,8 @@ class User < ApplicationRecord
                         uniqueness: { case_sensitive: false }
       validates :password, presence: true, length: { minimum: 6 }
       has_secure_password
+    
+     def feed
+        Micropost.where("user_id = ?", id)
+      end  
 end
